83d3f3518c9a6bd3af03f00dfb06da31b2e0195f
Fernando Vázquez <fernando.vz00@gmail.com>
2019-06-10 18:13:15 -0300

    adding react project

--- appController.js
+++
@@ -1,9 +0,0 @@
-import { ProductCard } from './src/components/product-card/product-card.js';
-
-window.changeView = changeView;
-
-function changeView(view) {
-    $('#content').append().load('src/components/' + view + '-card/' + view + '-card.html');
-}
-
-
